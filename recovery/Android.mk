088fe1cf90030cafd23b87acadcf6880cc00b2ee
LuK1337 <priv.luk@gmail.com>
2018-08-12 20:51:09 +0200

    op3: Fix build of librecovery_updater_op3
    Change-Id: I308fca4987b049266ebaae04de8b3e0efbf836ba

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,9 +7,18 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    $(call project-path-for,recovery) \
+    $(TARGET_OUT_HEADERS)/edify \
+    $(TARGET_OUT_HEADERS)/otautil \
+    $(TARGET_OUT_HEADERS)/updater \
     system/core/base/include
+
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_op3
 LOCAL_MODULE_TAGS := eng
+
+LOCAL_STATIC_LIBRARIES := \
+    libedify \
+    libotautil \
+    libupdater
+
 include $(BUILD_STATIC_LIBRARY)
